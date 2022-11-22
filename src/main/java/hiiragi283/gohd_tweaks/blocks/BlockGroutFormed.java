a2e167344b87518a0b3f677a4cb5a0884fa692e2
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2022-11-22 22:45:16 +0900

    Update build.gradle

--- src/main/java/hiiragi283/gohd_tweaks/blocks/BlockGroutFormed.java
+++ src/main/java/hiiragi283/gohd_tweaks/blocks/BlockGroutFormed.java
@@ -3,10 +3,7 @@ package hiiragi283.gohd_tweaks.blocks;
 import hiiragi283.gohd_tweaks.util.BlockCommon;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 
 public class BlockGroutFormed extends BlockCommon {
     public BlockGroutFormed() {
