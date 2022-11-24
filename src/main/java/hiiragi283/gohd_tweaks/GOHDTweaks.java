ee9bf66936a77d4fccea0aa21373236c37c08934
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2022-11-24 13:01:25 +0900

    Add Japanese coments

--- src/main/java/hiiragi283/gohd_tweaks/GOHDTweaks.java
+++ src/main/java/hiiragi283/gohd_tweaks/GOHDTweaks.java
@@ -13,30 +13,40 @@ import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+//Modの定義
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = Reference.DEPENDENCIES, acceptedMinecraftVersions = Reference.MC_VERSIONS)
 public class GOHDTweaks {
 
+    //Blockの定義
     public static final Block BlockGroutFormed = new BlockGroutFormed();
+    //Itemの定義
     public static final Item ItemBookSpawn = new ItemBookSpawn();
     public static final Item ItemBookSyntax = new ItemBookSyntax();
     public static final Item ItemGroutFormed = new ItemGroutFormed();
     public static final Item ItemPartsAssembly = new ItemPartsAssembly();
     public static final Item ItemRagiTicket = new ItemRagiTicket();
+    //ログ出力用
     public static final Logger LOGGER = LogManager.getLogger(Reference.MOD_ID);
+    //Proxyの定義
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;
+    //Instanceの定義
     @Mod.Instance(Reference.MOD_ID)
     public static GOHDTweaks Instance;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
+        //このクラスとイベントに登録
         MinecraftForge.EVENT_BUS.register(this);
+        //Blockの登録
         ForgeRegistries.BLOCKS.register(BlockGroutFormed);
+        //Itemの登録
         ForgeRegistries.ITEMS.register(ItemBookSpawn);
         ForgeRegistries.ITEMS.register(ItemBookSyntax);
         ForgeRegistries.ITEMS.register(ItemGroutFormed);
         ForgeRegistries.ITEMS.register(ItemPartsAssembly);
         ForgeRegistries.ITEMS.register(ItemRagiTicket);
+        //Modelの登録
         proxy.register();
     }
 }
--- src/main/java/hiiragi283/gohd_tweaks/util/BlockCommon.java
+++ src/main/java/hiiragi283/gohd_tweaks/util/BlockCommon.java
@@ -15,68 +15,80 @@ import java.util.Random;
 
 public class BlockCommon extends Block {
 
+    //Blockstateのプロパティの定義
     public static final PropertyInteger type4 = PropertyInteger.create("type", 0, 3);
     public static final PropertyInteger type8 = PropertyInteger.create("type", 0, 7);
     public static final PropertyInteger type16 = PropertyInteger.create("type", 0, 15);
+    //メタデータの最大値の定義
     public final int maxMeta;
 
+    //Blockを登録するメソッド
     public BlockCommon(Material Material, String ID, int max) {
-        super(Material);
-        this.setRegistryName(Reference.MOD_ID, ID);
-        this.setCreativeTab(CreativeTabs.DECORATIONS);
-        this.setDefaultState(this.blockState.getBaseState().withProperty(type16, 0));
-        this.setUnlocalizedName(ID);
-        this.maxMeta = max;
+        super(Material); //Materialを設定
+        this.setRegistryName(Reference.MOD_ID, ID); //IDの設定
+        this.setCreativeTab(CreativeTabs.DECORATIONS); //表示するクリエイティブタブの設定
+        this.setDefaultState(this.blockState.getBaseState().withProperty(type16, 0)); //デフォルトのBlockstateをtype16の0番に設定
+        this.setUnlocalizedName(ID); //翻訳キーをIDから取得する
+        this.maxMeta = max; //メタデータの最大値を代入
     }
 
+    //メタデータの最大値を返すメソッド
     public int getMaxMeta() {
+        //デフォルトは15が最大値
         return 15;
     }
 
-    //Blockstate
+    //Blockstateの登録をするメソッド
     @Override
     protected BlockStateContainer createBlockState() {
+        //type16を使用すると、Blockstateは"type=0"から"type=15"までが登録される
         return new BlockStateContainer(this, type16);
     }
 
+    //Blockstateからメタデータを取得するメソッド
     @Override
     public int getMetaFromState(IBlockState state) {
         int i = 0;
+        //type16をもとにBlockstateからメタデータを返す
         i = state.getValue(type16);
+        //メタデータが最大値よりも大きい場合、最大値が代入される
         if (i > maxMeta)
             i = maxMeta;
         return i;
     }
 
+    //メタデータからBlockstateを返すメソッド
     @Override
     public IBlockState getStateFromMeta(int meta) {
+        //ビット演算子でメタデータを0~5までに絞り込む
         int i = meta & 15;
+        //type16をもとに指定したメタデータからBlockstateを返す
         IBlockState state = this.getDefaultState().withProperty(type16, i);
         return state;
     }
 
-    @Override
-    public IBlockState getActualState(IBlockState state, IBlockAccess worldIn, BlockPos pos) {
-        return state;
-    }
-
-    //Item Drop
+    //Blockstateをもとにドロップするアイテムのメタデータを指定するメソッド
     @Override
     public int damageDropped(IBlockState state) {
+        //type16をもとにBlockstateからメタデータを返す
         int i = state.getValue(type16);
+        //メタデータが最大値よりも大きい場合、最大値が代入される
         if (i > maxMeta)
             i = maxMeta;
         return i;
     }
 
-    @Override
-    public int quantityDropped(Random random) {
-        return 1;
-    }
-
+    //ドロップするアイテムを指定するメソッド
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        //Blockstateからブロックを取得し、更にそこからアイテムを所得して返す
         return Item.getItemFromBlock(state.getBlock());
     }
 
+    //ドロップする確率を指定するメソッド
+    @Override
+    public int quantityDropped(Random random) {
+        //常にドロップさせるので1
+        return 1;
+    }
 }
