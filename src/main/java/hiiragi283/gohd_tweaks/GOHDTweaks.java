8475b74ead68b05cd0b25b1ad2c6cf874f742d9c
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2022-11-25 14:17:43 +0900

    Add new events

--- src/main/java/hiiragi283/gohd_tweaks/GOHDTweaks.java
+++ src/main/java/hiiragi283/gohd_tweaks/GOHDTweaks.java
@@ -8,6 +8,8 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.util.text.TextComponentTranslation;
@@ -18,9 +20,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Objects;
 
 //Modの定義
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = Reference.DEPENDENCIES, acceptedMinecraftVersions = Reference.MC_VERSIONS)
@@ -60,6 +66,50 @@ public class GOHDTweaks {
         proxy.register();
     }
 
+    //プレイヤーがログインすると呼ばれるevent
+    @SubscribeEvent
+    public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
+        //各値の取得
+        EntityPlayer player = event.player;
+        World world = player.world;
+        if (!world.isRemote) {
+            player.sendMessage(new TextComponentTranslation("text.gohd_tweaks.decoration_line.name"));
+            player.sendMessage(new TextComponentTranslation("text.gohd_tweaks.welcome.name"));
+            player.sendMessage(new TextComponentTranslation("text.gohd_tweaks.decoration_line.name"));
+        }
+    }
+
+    //各tickにおいてプレイヤーを対象に呼ばれるevent
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        //各値の取得
+        EntityPlayer player = event.player;
+        World world = player.world;
+        BlockPos pos = player.getPosition();
+        IBlockState blockstate = world.getBlockState(pos);
+        Block block = blockstate.getBlock();
+        //20tickに1回実行する (負荷対策)
+        if (world.getWorldTime() % 20 == 0) {
+            //プレイヤーが利き手にアイテムを持っている場合
+            if (Objects.isNull(player.getHeldItemMainhand())) {
+                //アイテムの値を取得する
+                ItemStack stackMain = player.getHeldItemMainhand();
+                Item itemMain = stackMain.getItem();
+            }
+            //プレイヤーが利き手ではない方の手にアイテムを持っている場合
+            if (Objects.isNull(player.getHeldItemOffhand())) {
+                //アイテムの値を取得する
+                ItemStack stackOff = player.getHeldItemOffhand();
+                Item itemOff = stackOff.getItem();
+            }
+            //プレイヤーと同じ座標にあるブロックのIDが"dcs_climate:dcs_fluidblock_hotspring"と一致する場合
+            if (block.getRegistryName().equals("dcs_climate:dcs_fluidblock_hotspring")) {
+                //プレイヤーに再生Iのバフを6秒与える
+                player.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("minecraft:regeneration"), 220, 0));
+            }
+        }
+    }
+
     //ブロックを右クリックすると呼ばれるevent
     @SubscribeEvent
     public void onRightClickBlock(RightClickBlock event) {
@@ -77,7 +127,7 @@ public class GOHDTweaks {
             if (item.getRegistryName().toString().equals("minecraft:book")) {
                 player.sendMessage(new TextComponentTranslation("text.gohd_tweaks.decoration_line.name"));
                 //ブロックの翻訳名をチャットに表示
-                player.sendMessage(new TextComponentString("§lName:§r " + block.getPickBlock(blockstate, player.rayTrace(0,0), world, pos, player).getDisplayName()));
+                player.sendMessage(new TextComponentString("§lName:§r " + block.getPickBlock(blockstate, player.rayTrace(0, 0), world, pos, player).getDisplayName()));
                 //ブロックのIDをチャットに表示
                 player.sendMessage(new TextComponentString("§lID:§r " + block.getRegistryName()));
                 //ブロックのHardnessをチャットに表示
